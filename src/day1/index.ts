65025868ac3041a6bd1e891012de65f7726765ba
Marcus Gardiner <marcus.gardiner@softwire.com>
2024-12-18 18:35:06 +0000

    extract getColumns for day 1 part 1 and 2

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,5 +1,17 @@
 import { Day } from "../day";
 
+const getColumns = (input: string) => {
+    let arrayOfLines: string[] = input.trim().split('\n');
+    let column1: number[] = [];
+    let column2: number[] = [];
+    arrayOfLines.forEach(line => {
+        let [column1number, column2number] = line.split(/\s+/).map(Number);
+        column1.push(column1number);
+        column2.push(column2number);
+    })
+    return {column1, column2};
+}
+
 class Day1 extends Day {
 
     constructor(){
@@ -7,14 +19,7 @@ class Day1 extends Day {
     }
 
     solveForPartOne(input: string): string {
-        let arrayOfLines: string[] = input.trim().split('\n');
-        let column1: number[] = [];
-        let column2: number[] = [];
-        arrayOfLines.forEach(line => {
-            let [column1number, column2number] = line.split(/\s+/).map(Number);
-            column1.push(column1number);
-            column2.push(column2number);
-        })
+        let {column1, column2} = getColumns(input);
 
         let sortedColumn1 = column1.sort((a, b) => a - b);
         let sortedColumn2 = column2.sort((a, b) => a - b);
@@ -25,18 +30,9 @@ class Day1 extends Day {
         return difference.toString()
     }
 
-    // adding up each number in the left list
-    // after multiplying it by the number of times that number appears in the right list.
-
     solveForPartTwo(input: string): string {
-        let arrayOfLines: string[] = input.trim().split('\n');
-        let column1: number[] = [];
-        let column2: number[] = [];
-        arrayOfLines.forEach(line => {
-            let [column1number, column2number] = line.split(/\s+/).map(Number);
-            column1.push(column1number);
-            column2.push(column2number);
-        })
+        let {column1, column2} = getColumns(input);
+
         let column1Multiplied = column1.map((x) => x * (column2.filter((v) => v === x).length));
         const total = column1Multiplied.reduce((acc, current) => acc + current, 0)
         return total.toString()
